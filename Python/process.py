ea85b216470686daaef841250efe3aa89c577811
Pravendra Singh <hackpravj@gmail.com>
2013-09-22 12:09:14 +0530

    added 'working process'

--- Python/process.py
+++ Python/process.py
@@ -1,1 +1,11 @@
 # working-process for validate
+
+# it tries to make a new email account in <entered email's domain>
+
+# if succeed, than <entered email> does not exists
+# --- and <entered email> is in-valid
+
+# else if not succeed, than <entered email> already exists
+# --- and <entered email> is valid
+
+# and it just not literally make a new email <It just try to, do so>
